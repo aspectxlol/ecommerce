cff16e5d165f1c4d90790f71073209a972b59fc2
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-11-14 20:31:33 +0700

    added routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import Products from "./routes/Products";
+import Orders from "./routes/Orders.ts";
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use("/products", Products);
+app.use("/orders", Orders);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
